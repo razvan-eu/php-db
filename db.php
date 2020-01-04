1c90f09dcfb4ba217bc1378d7bae711051772a9e
razvan-eu <razvan.peste@gmail.com>
2020-01-04 15:20:28 +0200

    Added descriptions to DB methods

--- db.php
+++ db.php
@@ -1,8 +1,15 @@
 <?
 class DB {
-
 	private $db;
 	
+	/**
+	 * __construct
+	 *
+	 * @param  mixed $DB_host
+	 * @param  mixed $DB_name
+	 * @param  mixed $DB_user
+	 * @param  mixed $DB_pass
+	 */
 	function __construct($DB_host, $DB_name, $DB_user, $DB_pass)
 	{
 	    try
@@ -17,15 +24,54 @@ class DB {
 	    }
 	}
 
-	public function select($table, $fields='*', $where='1=1', $order='', $limit = '') {
-	    $sql = "SELECT " . $fields . " FROM " . $table . " WHERE " . $where . "" . (!empty($order)? " ORDER BY " . $order : "") . "" .(!emptu($limit) ? 'LIMIT '.$imit : '');
+	/**
+	 * select - get table records / allow where, order and limit clauses
+	 *
+	 * @param  string $table
+	 * @param  string $fields
+	 * @param  string $where
+	 * @param  string $order
+	 *
+	 * @return array $result
+	 */
+	public function select($table, $fields = '*', $where = '1=1', $order = '', $limit = '') {
+		$sql = "SELECT ".$fields." FROM " . $table . " WHERE " . $where . "" ; 
+		$sql .= (!empty($order)? " ORDER BY " . $order : "");
+		$sql .= (!empty($limit)? " LIMIT " . $limit : "");
 		$stmt = $this->db->prepare($sql);
 	    $result=$stmt->execute();
 		$result=$stmt->fetchAll(PDO::FETCH_ASSOC);
 	    //$result=$stmt->fetchAll(PDO::FETCH_CLASS, 'person');
 	    return $result;
 	}
+	
+	/**
+	 * getRecordsNumber - get table record numbers / allow where condition
+	 *
+	 * @param  string $table
+	 * @param  string $fields
+	 * @param  string $where
+	 *
+	 * @return int $intRecords
+	 */
+	public function getRecordsNumber($table, $fields='*', $where='1=1') {
+	    $sql = "SELECT ".$fields." FROM " . $table . " WHERE " . $where . "";
+	    $stmt = $this->db->prepare($sql);
+	    $result=$stmt->execute();
+	    $intRecords=$stmt->rowCount();
+	    //$result=$stmt->fetchAll(PDO::FETCH_CLASS, 'person');
+	    return $intRecords;
+	}
+	
     
+    /**
+     * insert - insert record into table
+     *
+     * @param  string $table
+     * @param  array $array
+     *
+     * @return int $id
+     */
     public function insert($table, $array) {
         $fields=array_keys($array);
         $values=array_values($array);
@@ -44,6 +90,15 @@ class DB {
         return $id;
     }
 
+    /**
+     * update - update table record by id
+     *
+     * @param  string $table
+     * @param  mixed $array
+     * @param  int $id
+     *
+     * @return bool
+     */
     public function update($table, $array, $id) {
         $fields=array_keys($array);
         $values=array_values($array);
@@ -59,27 +114,40 @@ class DB {
         return $stmt->execute($values);
     }
 
+    /**
+     * delete - delete record from table by id
+     *
+     * @param  string $table
+     * @param  int $id
+     *
+     * @return bool
+     */
     public function delete($table, $id) {
         $sql = "DELETE FROM " . $table . " WHERE id=?";
         $stmt = $this->db->prepare($sql);
         return $stmt->execute(array($id));
     }
 	
+	/**
+	 * resetAutoIncrement - reset table auto increment
+	 *
+	 * @param  string $table
+	 *
+	 * @return bool
+	 */
 	public function resetAutoIncrement($table) {
         $sql = "ALTER TABLE " . $table . " AUTO_INCREMENT = 0";
         $stmt = $this->db->prepare($sql);
         return $stmt->execute(array($id));
 	}
-
-	public function getRecordsNumber($table, $fields='*', $where='1=1') {
-	    $sql = "SELECT ".$fields." FROM " . $table . " WHERE " . $where . "";
-	    $stmt = $this->db->prepare($sql);
-	    $result=$stmt->execute();
-	    $intRecords=$stmt->rowCount();
-	    //$result=$stmt->fetchAll(PDO::FETCH_CLASS, 'person');
-	    return $intRecords;
-	}
-		
+	
+	/**
+	 * getColumns - get table columns
+	 *
+	 * @param  string $table
+	 *
+	 * @return array $output
+	 */
 	public function getColumns($table){
 		$sql = "SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH, COLUMN_TYPE, COLUMN_KEY, EXTRA FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = :table";
 		try {
@@ -98,6 +166,13 @@ class DB {
 		}
 	}	
 
+	/**
+	 * getColumnNames - get tablel column names
+	 *
+	 * @param  string $table
+	 *
+	 * @return array $output
+	 */
 	public function getColumnNames($table){
 		$sql = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = :table";
 		try {
